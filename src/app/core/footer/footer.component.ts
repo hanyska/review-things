39cb7aaf8d4289900283bd496fce4fb1f2ec1f57
hanyska <anna.janusz@loycon.pl>
2020-07-13 20:35:06 +0200

    Updated footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'rt-footer',
   template: `
-    <p>
+    <div>
       <span class="pr-1">{{'footer.createdWith' | translate}}</span>
       <nb-icon icon="heart" [options]="{ animation: { type: 'zoom' } }"></nb-icon>
       <span class="px-1">{{'footer.by' | translate}}</span>
       <span>Anna Janusz</span>
-    </p>
+    </div>
   `
 })
 export class FooterComponent {}
