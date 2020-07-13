4ad8bfe01153a95def2dc0ff8b4be949834ba6fe
hanyska <anna.janusz@loycon.pl>
2020-07-13 20:36:16 +0200

    Updated header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,8 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'rt-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  template: `
+    <nav class="d-flex justify-content-between">
+      <div>
+        <a class="text-decoration-none" [routerLink]="['/']">
+          <h1>{{'appName' | translate}}</h1>
+        </a>
+      </div>
+      <div>
+        <button nbButton hero status="primary" [routerLink]="['/auth/login']">Login</button>
+        <button nbButton hero status="danger">{{'header.logout' | translate}}</button>
+      </div>
+    </nav>
+  `
 })
 export class HeaderComponent implements OnInit {
 
